036cbaf73793121f8d05f5ccaeacb396a6631002
humoflife <markus.schweig@upbound.io>
2025-03-14 14:41:41 -0700

    iterate on xpprovider

--- xpprovider/xpprovider.go
+++ xpprovider/xpprovider.go
@@ -3,10 +3,9 @@ package xpprovider
 import (
 	"context"
 
-	"github.com/aliyun/terraform-provider-alicloud/internal/providers/sdkv2"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 )
 
 func GetProvider(ctx context.Context) (*schema.Provider, error) {
-	return sdkv2.AlibabaCloudProvider(), nil
+	return GetSchema(), nil
 }
