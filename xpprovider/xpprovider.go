8ae205e2d18b9910a646539380707ea00e004dda
humoflife <markus.schweig@upbound.io>
2025-03-14 15:32:37 -0700

    call alicloud.Provider()

--- xpprovider/xpprovider.go
+++ xpprovider/xpprovider.go
@@ -3,9 +3,10 @@ package xpprovider
 import (
 	"context"
 
+	"github.com/aliyun/terraform-provider-alicloud/alicloud"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 )
 
 func GetProvider(ctx context.Context) (*schema.Provider, error) {
-	return GetSchema(), nil
+	return alicloud.Provider(), nil
 }
