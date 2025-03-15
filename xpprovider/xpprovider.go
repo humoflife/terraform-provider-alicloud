b695e58290301c11b51a2d7de1cfea573ada435b
humoflife <markus.schweig@upbound.io>
2025-03-15 14:40:28 -0700

    add alicloud.ProviderConfigure(d, p)

--- xpprovider/xpprovider.go
+++ xpprovider/xpprovider.go
@@ -5,8 +5,16 @@ import (
 
 	"github.com/aliyun/terraform-provider-alicloud/alicloud"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	schemaV2 "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
-func GetProvider(_ context.Context) (*schema.Provider, error) {
-	return alicloud.Provider().(*schema.Provider), nil
+func GetProviderSchema(_ context.Context) (*schemaV2.Provider, error) {
+	var d *schema.ResourceData
+	var p *schema.Provider
+	var pV2 *schemaV2.Provider
+
+	p = alicloud.Provider().(*schema.Provider), nil
+	alicloud.ProviderConfigure(d, p)
+	pV2.Schema = p.Schema
+	return pV2
 }
