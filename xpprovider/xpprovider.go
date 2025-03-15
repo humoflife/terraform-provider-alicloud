a5a3f582500386e594dc0229cd734f0215c2de2f
humoflife <markus.schweig@upbound.io>
2025-03-14 20:59:31 -0700

    add *schema.Provider type assertion

--- xpprovider/xpprovider.go
+++ xpprovider/xpprovider.go
@@ -4,10 +4,9 @@ import (
 	"context"
 
 	"github.com/aliyun/terraform-provider-alicloud/alicloud"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	// "github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 )
 
 func GetProvider(_ context.Context) (*schema.Provider, error) {
-	return alicloud.Provider(), nil
+	return alicloud.Provider().(*schema.Provider), nil
 }
